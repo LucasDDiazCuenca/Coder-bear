e6ae7d094073b90c1e11397dd2705a1926f2e59a
Flors Rueda <ruedaflors@gmail.com>
2024-04-04 11:22:24 +0200

    #04 add class in-code documentation

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,9 +1,6 @@
-/* Class Constructor Validator */
+// Class Constructor Validator
 
 import { ENHANCEMENTS, MISSIONS, maxUpdateValues, startingUpdateValues } from "./constants";
-
-/**TODO: documentate */
-
 type Missions = { [key: string]: boolean };
 
 const validateAttribute = (attr: string, value: any): void => {
@@ -25,7 +22,9 @@ const validateAttribute = (attr: string, value: any): void => {
     }
 }
 
-/* Add class documentation */
+/**
+ * Represents a game instance with all its attributes and functionalities.
+ */
 export default class Game {
     money: number;
     happiness: number;
@@ -40,6 +39,21 @@ export default class Game {
     idleMoney: number;
     missionProgress: number;
 
+    /**
+     * Constructor for creating a new game instance.
+     * @param money Initial amount of money.
+     * @param happiness Initial level of happiness.
+     * @param hunger Initial level of hunger.
+     * @param missions Object containing mission statuses.
+     * @param enhancements Array containing unlocked enhancements.
+     * @param recoverHunger Amount of hunger recovered over time.
+     * @param loseHunger Amount of hunger lost over time.
+     * @param recoverHappiness Amount of happiness recovered over time.
+     * @param loseHappiness Amount of happiness lost over time.
+     * @param clickMoney Amount of money gained per click.
+     * @param idleMoney Amount of money gained while idle.
+     * @param missionProgress Progress towards the current mission.
+    */
     constructor(
         money: number = 50,
         happiness: number = 90,
@@ -80,21 +94,37 @@ export default class Game {
         this.missionProgress = missionProgress
     }
 
+    /**
+     * Update the current amount of money.
+     * @param value The amount to add to the money.
+    */
     updateMoney(value: number): void {
         if (this.money + value < 0) throw new Error('Invalid operation. Money can not be less than 0.');
         this.money = this.money + value;
     }
 
+    /**
+     * Update the current level of happiness.
+     * @param value The amount to add/remove to happiness.
+    */
     updateHappiness(value: number): void {
         if (0 <= this.happiness + value && this.happiness + value <= 100) this.happiness = this.happiness + value
         else this.happiness = 0 > this.happiness + value ? 0 : 100
     }
 
+    /**
+     * Update the current level of hunger.
+     * @param value The amount to add/remove to hunger.
+    */
     updateHunger(value: number): void {
         if (0 <= this.hunger + value && this.hunger + value <= 100) this.hunger = this.hunger + value
         else this.hunger = 0 > this.hunger + value ? 0 : 100
     }
 
+    /**
+     * Update the parameter to update recover-hunger.
+     * @param value The amount to add to recover-hunger.
+    */
     updateRecoverHunger(value: number): void {
         if (value <= 0) throw new Error('Invalid operation. Value is not valid.');
         if (this.recoverHunger + value > maxUpdateValues['recover-hunger']) throw new Error('Invalid operation. Value will overpass max.');
@@ -102,6 +132,10 @@ export default class Game {
         this.recoverHunger += value;
     }
 
+    /**
+     * Update the parameter to update lose-hunger.
+     * @param value The amount to remove to lose-hunger.
+    */
     updateLoseHunger(value: number): void {
         if (value <= 0) throw new Error('Invalid operation. Value is not valid.');
         if (this.loseHunger - value < maxUpdateValues['lose-hunger']) throw new Error('Invalid operation. Value will overpass max.');
@@ -109,6 +143,10 @@ export default class Game {
         this.loseHunger -= value;
     }
 
+    /**
+     * Update the parameter to update recover-happiness.
+     * @param value The amount to add to recover-happiness.
+    */
     updateRecoverHappiness(value: number): void {
         if (value <= 0) throw new Error('Invalid operation. Value is not valid.');
         if (this.recoverHappiness + value > maxUpdateValues['recover-happiness']) throw new Error('Invalid operation. Value will overpass max.');
@@ -116,6 +154,10 @@ export default class Game {
         this.recoverHappiness += value;
     }
 
+    /**
+     * Update the parameter to update lose-happiness.
+     * @param value The amount to remove to lose-happiness.
+    */
     updateLoseHappiness(value: number): void {
         if (value <= 0) throw new Error('Invalid operation. Value is not valid.');
         if (this.loseHappiness - value < maxUpdateValues['lose-happiness']) throw new Error('Invalid operation. Value will overpass max.');
@@ -123,18 +165,30 @@ export default class Game {
         this.loseHappiness -= value;
     }
 
+    /**
+     * Update the parameter to update click-money.
+     * @param value The amount to add to click-money.
+    */
     updateClickMoney(value: number): void {
         if (value <= 0) throw new Error('Invalid operation. Value is not valid.');
 
         this.clickMoney += value;
     }
 
+    /**
+     * Update the parameter to update idle-money.
+     * @param value The amount to add to idle-money.
+    */
     updateIdleMoney(value: number): void {
         if (value <= 0) throw new Error('Invalid operation. Value is not valid.');
 
         this.idleMoney += value;
     }
 
+    /**
+     * Get the id of the mission active or none in case of no mission active.
+     * @returns An string with the id/none.
+    */
     getMissionInProgress(): string {
         for (const mission in this.missions) {
             if (this.missions.hasOwnProperty(mission) && this.missions[mission] === false) {
@@ -144,6 +198,12 @@ export default class Game {
         return 'none';
     }
 
+    /**
+     * Update the missions object.
+     * If the mission id is not active it sets it up as false.
+     * If the mission id is active it sets it up as true (complete) and resets the missionProgress.
+     * @param mission The mission id.
+    */
     updateMissions(mission: string): void {
         if ((MISSIONS.map(({ id }) => id)).includes(mission) === false) throw new Error('Invalid operation. Mission does not exist.');
         if (Object.values(this.missions).some(complete => !complete) && this.missions[mission] === undefined) throw new Error('Invalid operation. Another mission is already active.');
@@ -159,6 +219,10 @@ export default class Game {
         }
     }
 
+    /**
+     * Delete a mission of the array if it's not complete (false).
+     * @param mission The mission id.
+    */
     deleteMission(mission: string): void {
         if ((MISSIONS.map(({ id }) => id)).includes(mission) === false) throw new Error('Invalid operation. Mission does not exist.');
         if (this.missions[mission] === undefined) throw new Error('Invalid operation. Mission is not active.');
@@ -168,6 +232,10 @@ export default class Game {
         this.missionProgress = 0;
     }
 
+    /**
+     * Update the current level of mission progress if a mission is in progress. If it reaches the mission goal marks this mission as complete and resets the mission progress.
+     * @param value The amount to add to mission-progress.
+    */
     updateMissionProgress(value: number): void {
         if (value <= 0) throw new Error('Invalid operation. Value is not valid.');
 
@@ -183,6 +251,10 @@ export default class Game {
         }
     }
 
+    /**
+     * Update the enhancements array and updates values related to that enhancement.
+     * @param enhancement The enhancement id.
+    */
     updateEnhancements(enhancement: string): void {
         if ((ENHANCEMENTS.map(({ id }) => id)).includes(enhancement) === false) throw new Error('Invalid operation. Enhancement does not exist.');
         if (this.enhancements.includes(enhancement)) throw new Error('Invalid operation. Enhancement is already unlocked.');
@@ -212,6 +284,9 @@ export default class Game {
         this.enhancements.push(enhancement)
     }
 
+    /**
+     * Applies all effects relative to a click in game depending of if there's a mission active.
+    */
     gameOnClick(): void {
         if (this.hunger === 0) throw new Error('Invalid operation. Not available with hunger === 0.');
         const updateOnClick = this.happiness === 0 ? this.clickMoney / 2 : this.clickMoney;
@@ -226,6 +301,9 @@ export default class Game {
         }
     }
 
+    /**
+     * Applies all effects relative to idle in game depending of if there's a mission active.
+    */
     gameOnIdle(): void {
         if (Object.values(this.missions).some(complete => !complete)) {
             this.updateHappiness(this.recoverHappiness);
@@ -237,6 +315,10 @@ export default class Game {
         }
     }
 
+    /**
+     * Get the data of the game instance necesary to save the whole game.
+     * @returns An object containing the game data.
+    */
     getGameData(): object {
         return {
             money: this.money,
@@ -254,6 +336,10 @@ export default class Game {
         }
     }
 
+    /**
+     * Get the basic stats of the game instance.
+     * @returns An object containing basic game stats (money, happiness, hunger).
+    */
     getGameStats(): object {
         return {
             money: this.money,
@@ -262,6 +348,10 @@ export default class Game {
         }
     }
 
+    /**
+     * Get the update values of the game instance.
+     * @returns An object containing the update values (recoverHunger, loseHunger, etc.).
+    */
     getGameUpdateValues(): object {
         return {
             recoverHunger: this.recoverHunger,
@@ -273,6 +363,10 @@ export default class Game {
         }
     }
 
+    /**
+     * Get the list of completed missions.
+     * @returns An array containing the ids of completed missions.
+    */
     getCompleteMissions(): Array<string> {
         const completeMissions: Array<string> = [];
         for (const mission in this.missions) {
