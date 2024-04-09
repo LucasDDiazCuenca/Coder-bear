b9435e86b157c54bcbebb3873ac4095cb6b89d8a
Flors Rueda <ruedaflors@gmail.com>
2024-04-09 12:58:25 +0200

    #6 add game top-bar to interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import HelloWorld from "./components/HelloWorld";
 import ScenarioExperience from "./components/ScenarioExperience";
 import { Canvas } from "@react-three/fiber";
+import useGame from "./game/useGame";
+import GameTopBar from "./components/GameTopBar";
+import { useEffect, useState } from "react";
 
 const App = (): React.ReactElement => {
+	const { game, missionsList, enhancementsList, saveGame } = useGame();
+	const [missionsActivated, setMissionsActivated] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (game?.getMissionInProgress() !== 'none') setMissionsActivated(true)
+	}, [])
+
+	/*const handleActivateMissions = (id: string) => {
+		game?.updateMissions(id)
+		saveGame();
+		setMissionsActivated(true);
+	}*/
+
 	return (
 		<>
 			<div className="w-screen h-screen text-center text-3xl">
+				{game && <GameTopBar game={game} isMissionActive={missionsActivated} missions={missionsList} />}
 				<div className="h-5/6">
 					<Canvas
 						dpr={[1, 2]}
